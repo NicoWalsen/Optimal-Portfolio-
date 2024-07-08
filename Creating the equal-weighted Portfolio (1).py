0cc5271e078e22adc49af46701eef8c7ccfe7b0e
Nicolás Walsen <160145060+NicoWalsen@users.noreply.github.com>
2024-07-08 08:27:02 -0400

    Update Creating the equal-weighted Portfolio (1).py

--- Creating the equal-weighted Portfolio (1).py
+++ Creating the equal-weighted Portfolio (1).py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import yfinance as yf
 pd.options.display.float_format = '{:.2f}'.format
 
 #We download the ticker data from Yahoo Finance with the method yf.download().
@@ -44,7 +45,7 @@ summary.Return = summary.Return*252
 summary.Risk = summary.Risk*np.sqrt(252) 
 summary
 
-#We plot each of our stocks and equal weighted portfolio
+#Plot each stocks and the equal weighted portfolio
 summary.plot(kind = "scatter", x= "Risk", y = "Return", figsize = (13,9), s= 50, fontsize = 15)
 for i in summary.index:
     plt.annotate(i, xy=(summary.loc[i, "Risk"]+0.002, summary.loc[i,"Return"]+0.002), size=15)
@@ -68,28 +69,29 @@ noa = len(stocks.columns)
 noa #number of assets
 nop = 100000
 nop #number of random porfolios 
-#We will create random list with numbers with 10 raws and 6 columns.
+
+#We will create random list with numbers with 10 rows and 6 columns.
 np.random.random(10*6).reshape(10,6) 
-#If we run the previous cell once again, we will get different numbers each time. To avoid this we must include np.random.seed() method.
+#If we run the previous cell once again, we will get different numbers each time. To avoid this issue, we can use ".random.seed()" method.
 np.random.seed(123) # If we run the previous code again, we will get always the same numbers [if we pass the number "123" to the method np.random.seed()]
 #we run again the same code that before but using "noa" and "nop"
 matrix = np.random.random(noa*nop).reshape(nop,noa)
 matrix
-#We generate the sum for each raw of the previous matrix (axis=1). Keepdims = True means that we will mantain the same dimentions of the origin matrix.
+#We generate the sum of each row for the previous matrix (axis=1). Keepdims = True means that we will mantain the same dimentions of the origin matrix.
 matrix.sum(axis=1, keepdims=True) 
 
 #Now, we are going to normalize the values of the matrix "matrix". In order to do it, we will divide each of the values of the raws by the value calculated en each raws on the matrix of 1 columns and 10 raws.
 #Moreover, we obtain that the sum of each file is equal to 1 and we acomplish the first restriction for our portfolios.
 weights = matrix / matrix.sum(axis=1, keepdims=True)
 weights 
-weights.sum(axis=1, keepdims=True) #demostrating that the sum of each raw is equal to 1.
+weights.sum(axis=1, keepdims=True) #sum of each row is equal to 1.
 
 port_ret = ret.dot(weights.T) #We multiply the matrix returns with transposed weights in order to obtain the daily returns for each of our 10 portfolios. 
 port_ret
 port_summary = ann_risk_return(port_ret) #our function will provide both the anualized return and risk for each of our 100.000 portfolios.
 port_summary 
 
-# We graph the stocks and equal weighted portfolio and we also add our 100.000 random portfolios. 
+# Plot the stocks and equal weighted portfolio and include 100.000 random portfolios. 
 plt.figure(figsize = (15,9))
 plt.scatter(port_summary.loc[:,"Risk"], port_summary.loc[:,"Return"], s=20, color = "r")
 plt.scatter(summary.loc[:,"Risk"], summary.loc[:,"Return"], s=50, color ="black", marker ="D")
@@ -114,8 +116,8 @@ port_summary["Sharpe"] = (port_summary["Return"]-rf[0])/port_summary["Risk"]
 port_summary
 port_summary.describe()
 
-#We plot our 100.000 stocks and their Sharpe Ratio, higher sharpe ratios are strong red and lower are strong blue.
-#We also graph our 6 initial stocks.
+#Plot our 100.000 stocks and their Sharpe Ratio, higher sharpe ratios are colored with strong red and lower with strong blue.
+#We will also plot 6 initial stocks.
 plt.figure(figsize = (15,8))
 plt.scatter(port_summary.loc[:,"Risk"], port_summary.loc[:,"Return"], s=20,
             c=port_summary.loc[:,"Sharpe"], cmap = "coolwarm", vmin = 0.75, vmax = 1.18, alpha = 0.8)
@@ -131,11 +133,11 @@ plt.show()
 port_summary.head()
 port_summary.describe()
 weights
-msrp = port_summary.Sharpe.idxmax() #We will get the raw number with the higher Sharpe Ratio with the method .idmax()
+msrp = port_summary.Sharpe.idxmax() #We will get the row number with the higher Sharpe Ratio using the method .idmax()
 msrp
-msrp_p = port_summary.iloc[msrp] #We found that the higher Sharpe ratio is in the raw # is 76879, also, we get the descriptive statistics.
+msrp_p = port_summary.iloc[msrp] #We found the higher Sharpe ratio in the row #76879 with its descriptive statistics.
 msrp_p
 msrp_w = weights[msrp,:] # With this we obtain the weights for this portfolio.
 msrp_w
-#Finally we get the weights for each assets that represents our random portfolio with the maximum Sharpe ratio.
+#Finally we get the weights for each assets of the portfolio with the maximum Sharpe ratio.
 pd.Series(index=stocks.columns, data = msrp_w) 
